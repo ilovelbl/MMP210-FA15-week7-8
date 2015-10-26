002c51b92e7fbfe5fd4bfcb1c93da98b89385b03
ilovelbl <siyu.han@f1006-23-34167.bmcc.cuny.edu>
2015-10-26 15:18:20 -0400

    ver.1.4

--- Week7-inClass-10.26.15-Start/js/main.js
+++ Week7-inClass-10.26.15-Start/js/main.js
@@ -8,13 +8,21 @@ function setup(){
     var myArray = [];//creat blank array
     //add stuff to the array with a loop
     for (var i=0; i<10000; i++){
-        myArray[i] = "item" + i;
+// ---------       myArray[i] = "item" + i;
+        myArray[i] = i;
     }
-    console.log(myArray[0]);
-    console.log(myArray[1]);
-    console.log(myArray[2]);
-    console.log(myArray);
-    console.log("myArray", myArray);
+// ---------   console.log(myArray[0]);
+// ---------   console.log(myArray[1]);
+// ---------   console.log(myArray[2]);
+// ---------   console.log(myArray);
+// ---------   console.log("myArray", myArray);
 
+    //log out the array
+    console.log(myArray);
+
+    // Loop to go through every item in the array
+    for(var i=0; i < myArray.length; i++){
+        myArray[i]+=100;
+    }
 }//end setup
 
