2f1d277246c12d6df6f67634ed51fa49e03cf41e
Siyu Han <Siyu Han>
2015-11-23 15:27:26 -0500

    11 1st

---
+++ Week11/js/main.js
@@ -0,0 +1,10 @@
+//var Penguins;
+//function preload(){
+//    var url = https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/birds_antarctica.json;
+//    penduins = ;loadjson (url)
+//}
+
+function setup (){
+    var cnv = create Canvas (windowWidth, windowHeight);
+    cnv.parent("canvas");
+}
