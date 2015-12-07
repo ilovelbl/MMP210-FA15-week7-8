28636a0acade635358c70e3ce5a125a88b961b73
ilovelbl <siyu.han@f1006-23-34167.bmcc.cuny.edu>
2015-12-07 17:10:46 -0500

    1st Ver. of final (jump only)

--- Final/js/main.js
+++ Final/js/main.js
@@ -5,25 +5,24 @@ var GRAVITY = 1;
 var JUMP = 20;
 
 function setup() {
-    createCanvas(800,600);
+    createCanvas(windowWidth,windowHeight);
 
-    cowboy = createSprite(200,200);
+    cowboy = createSprite(200,400);
     cowboy.addAnimation("nomal","assets/cb-s1.png", "assets/cb-s2.png");
-    cowboy.addAnimation("stretch","assets/asterisk_stretching0001.png", "assets/asterisk_stretching0008.png");
+    cowboy.addAnimation("stretch","assets/cb-jp1.png","assets/cb-jp1.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png");
 
       //if defined, the collider will be used for mouse events
     cowboy.setCollider("circle", 0,0,134);
 
-    platform = createSprite(200, 400);
-    platform.addAnimation("normal", "assets/small_platform0001.png",  "assets/small_platform0003.png");
+    platform = createSprite(400, 800);
+    platform.addAnimation("normal", "assets/grd.png");
 }
 
 function draw() {
     background(255,255,255);
 
     fill (200);
-//    textAlign(CENTER);
-//    text("Welcome to the Final",400,20);
+
     cowboy.velocity.y += GRAVITY;
     if(cowboy.collide(platform)){
         cowboy.velocity.y = 0;
