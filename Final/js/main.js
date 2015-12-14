d73f76fe4f4a325aa25a8b11aef1ca1863441851
ilovelbl <siyu.han@f1006-23-34167.bmcc.cuny.edu>
2015-12-14 16:09:08 -0500

    shot down with flashing bugs

--- Final/js/main.js
+++ Final/js/main.js
@@ -7,17 +7,16 @@ var JUMP = 20;
 function setup() {
     createCanvas(windowWidth,windowHeight);
 
-    cowboy = createSprite(200,400);
+    cowboy = createSprite(200,610);
     cowboy.addAnimation("nomal","assets/cb-s1.png", "assets/cb-s2.png");
     cowboy.addAnimation("stretch","assets/cb-jp1.png","assets/cb-jp1.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png", "assets/cb-jp2.png");
+    cowboy.addAnimation("died","assets/cb-die1.png","assets/cb-die2.png","assets/cb-die3.png","assets/cb-die4.png","assets/cb-die5.png","assets/cb-die6.png","assets/cb-die6.png","assets/cb-die6.png","assets/cb-die6.png","assets/cb-die6.png","assets/cb-die6.png","assets/cb-die6.png");
 
-    cowboy.setCollider("rectangle", 0,0,20,253);
+    cowboy.setCollider("rectangle", 0,0,0,253);
 
 
     bullets = createSprite(800,600);
-    bullets.addAnimation("shoot","assets/small_circle0001.png");
-
-    bullets.setCollider("po",100,100,1000)
+    bullets.addAnimation("shoot","assets/b1.png");
 
     platform = createSprite(400, 800);
     platform.addAnimation("normal", "assets/grd.png");
@@ -37,11 +36,13 @@ function draw() {
     bullets.setSpeed(7,180);
     if (bullets.collide(cowboy)){
         bullets.setSpeed(0,0);
+
+        cowboy.changeAnimation("died");
     }
 
 
 
-  if(keyWentDown("x"))
+  if(keyWentDown("a"))
     {
     cowboy.changeAnimation("stretch");
     cowboy.animation.rewind();
